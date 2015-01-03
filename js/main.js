967ae7704965939f9d3f1d787fcc7773b4579cc3
Teemu Taskula <teemu.taskula@aalto.fi>
2015-01-03 21:51:52 +0200

    updated todo list

--- js/main.js
+++ js/main.js
@@ -1,13 +1,50 @@
 $( document ).ready(function() {
 	
+
+
+
+	// määrittele mitä ohjekuplassa näkyy
+	var triggers = $(".guide-trigger");
+	var all_guide_content = $(".guide-content");
+	var vh = $(window).height();
+	var of;
+
 	$( ".row1" ).scroll( function(){
-		if( $(".row1").scrollTop() >= ($(window).height() - 100) ){
+		
+		if( $(".row1").scrollTop() >= (vh-70) ){
 			$(".guide-wrapper").fadeIn();
+
+			triggers.each(function(i){
+				of = $(this).offset();
+				// kun skrollataan of.top ei saa jatkuvasti arvoja
+				// minkä takia täytyy testata lukuväliä
+				if ( of.top < (70) && of.top > (60)){
+					showGuideContent(i);
+				}
+			});
 		}
 		else{
 			$(".guide-wrapper").fadeOut();
 		}
 	});
+
+
+	function showGuideContent( index ){
+		all_guide_content.each(function(j){
+			if (j === index){
+				$(this).fadeIn()
+			}
+			else{
+				$(this).hide()
+			}
+		});
+	}
+
+
+
+
+
+
 
 	$(".next").click(function(){
 		var currentIndex = $(".current").data("order");
